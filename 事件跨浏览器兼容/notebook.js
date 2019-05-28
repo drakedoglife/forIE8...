54bff71781f187354d7a587e433c750fbe40f69a
高一方 <sakuram@gaoyifangdeMacBook-Pro.local>
2019-05-28 23:07:31 +0800

    增加event相关函数

--- 事件跨浏览器兼容/notebook.js
+++ 事件跨浏览器兼容/notebook.js
@@ -1,5 +1,6 @@
 // 事件兼容所有浏览器
 var EventUntil = {
+	// 添加事件
 	addHandler: function(element, type, handler) {
 		if (element.addEventListener) {
 			element.addEventListener(type, handler, false);
@@ -8,7 +9,8 @@ var EventUntil = {
 		}else {
 			element["no" + type] = null;
 		}
-	}
+	},
+	// 移除事件
 	removeHandler: function(element, type, handler) {
 		if (element.removeEventListener) {
 			element.removeEventListener(type, handler, false);
@@ -16,6 +18,26 @@ var EventUntil = {
 			element.detachEvent("on" + type, handler);
 		}else {
 			element["on" + type] = null;
+		}
+	},
+	// 得到event目标
+	getTarget: function(event) {
+		return event.target || event.srcElement;
+	},
+	// 阻止默认行为
+	preventDefault: function(event) {
+		if (event.preventDefault) {
+			evet.preventDefault();
+		}else {
+			event.returnValue = false;
+		}
+	},
+	// 停止冒泡或委托
+	stopPropagation: function(event) {
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		}else {
+			event.cancelBubble = true;
 		}
 	}
 }
